997a3203fe5c5fc9e351175315ecd4e5080ecead
Riddhima0405 <181880323+Riddhima0405@users.noreply.github.com>
2026-03-15 22:46:12 +0530

    Create IsomorphicStrings.py

---
+++ IsomorphicStrings.py
@@ -0,0 +1,19 @@
+def isIsomorphic(self, s: str, t: str) -> bool:
+        len(s) != len(t)
+        map_st = {}
+        map_ts = {}
+        for i in range(len(s)):
+
+            c1 = s[i]
+            c2 = t[i]
+
+            if c1 in map_st and map_st[c1] != c2:
+                return False
+
+            if c2 in map_ts and map_ts[c2] != c1:
+                return False
+
+            map_st[c1] = c2
+            map_ts[c2] = c1
+
+        return True
