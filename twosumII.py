6720e6c0099f8a3ea07c961950d87f8b34802fee
Riddhima0405 <181880323+Riddhima0405@users.noreply.github.com>
2026-03-20 23:37:49 +0530

    Create twosumII.py

---
+++ twosumII.py
@@ -0,0 +1,12 @@
+def twoSum(numbers, target):
+    left, right = 0, len(numbers) - 1
+    
+    while left < right:
+        s = numbers[left] + numbers[right]
+        
+        if s == target:
+            return [left + 1, right + 1]  # 1-based index
+        elif s < target:
+            left += 1
+        else:
+            right -= 1
